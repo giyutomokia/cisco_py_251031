9d2ce182624bf04da5a01dff095f74f27fc11d94
giyutomokia <137532030+giyutomokia@users.noreply.github.com>
2025-11-03 14:13:49 +0530

    new code

---
+++ pytttt.py
@@ -0,0 +1,27 @@
+from statistics import mean
+
+a = input()                     
+b = a.split()                   
+c = [float(x) for x in b]       
+d = tuple(c)                    
+
+print(c)
+print(d)
+
+summes = sum(c)                 
+avg = mean(c)
+
+print(summes)
+print(avg)
+
+with open("pytttt.txt", "w") as outputfile:
+    outputfile.write(str(c))
+    
+
+with open("pytttt.txt", "r") as outputfile:
+    
+    print(outputfile.read())
+
+
+    
+    
