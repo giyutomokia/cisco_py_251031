daf4b5e085927f139e62ff904bd3d4fbabb80e89
giyutomokia <137532030+giyutomokia@users.noreply.github.com>
2025-11-04 12:16:31 +0530

    log

--- app_json/repo.py
+++ app_json/repo.py
@@ -1,4 +1,5 @@
 import db_json as db 
+from log import logging
 
 employees = db.read_employees() #employee is object of attr (id, name, job_title, salary)
 # list of objects -> list of dict -> save to file
@@ -7,14 +8,16 @@ employees = db.read_employees() #employee is object of attr (id, name, job_title
 def add_employee(employee):
     employees.append(employee)
     db.write_employees(employees)
-    print('Employee Added Successfully')
+    logging.info('Employee Added Successfully')
 
 def search_employee(id): 
     I = 0
     for employee in employees: 
         if employee.id == id:
+            logging.info('Employee Exist')
             return I
         I += 1
+        logging.info('Employee Not Exist')
     return -1 
  
 def update_employee(id, salary):
@@ -23,15 +26,15 @@ def update_employee(id, salary):
         employee = employees[index]
         employee.salary = salary
         db.write_employees(employees)
-        print('Employee Updated Successfully')
+        logging.info('Employee Updated Successfully')
     else: 
-        print('Employee Not Found.')
+        logging.info('Employee Not Found.')
 
 def delete_employee(id):
     index = search_employee(id) 
     if index != -1:
         employees.pop(index)
         db.write_employees(employees)
-        print('Employee Deleted Successfully')
+        logging.info('Employee Deleted Successfully')
     else: 
-        print('Employee Not Found.')
+        logging.info('Employee Not Found.')
